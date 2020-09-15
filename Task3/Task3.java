935fce1f1ea91b869ca71bd12ca07ae55913d587
chistyakov <langlydes@gmail.com>
2020-09-15 16:05:48 +0300

    TASK3

--- Task3/Task3.java
+++ Task3/Task3.java
@@ -19,11 +19,21 @@ public class Task3 {
         Spaceship enterprise = new Spaceship(700);
         Spaceship milleniumFalcon = new Spaceship(750);
         Spaceship hyperion = new Spaceship(1000);
-
         int[] speeds = new int[]{enterprise.getSpeed(), milleniumFalcon.getSpeed(), hyperion.getSpeed()};
 
+        System.out.println("Case 1: ");
+        System.out.println("Number of ships: " + speeds.length);
         System.out.println("Average speed: " + avg(speeds));
         System.out.println("Minimum speed: " + min(speeds));
+        System.out.println("-----------------------------------------------------");
 
+        Spaceship ufo = new Spaceship(Integer.MAX_VALUE - 1);
+        int[] speedsMax = new int[]{enterprise.getSpeed(), milleniumFalcon.getSpeed(), hyperion.getSpeed(), ufo.getSpeed()};
+
+        System.out.println("Case 2: ");
+        System.out.println("Number of ships: " + speedsMax.length);
+        System.out.println("Average speed: " + avg(speedsMax));
+        System.out.println("Minimum speed: " + min(speedsMax));
+        System.out.println("-----------------------------------------------------");
     }
 }
