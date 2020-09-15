8877c41addf253ef117b38da0d1645c6def35de2
chistyakov <langlydes@gmail.com>
2020-09-15 14:56:29 +0300

    TASK1

---
+++ Task1/Task1.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Task1 {
+
+    static boolean swap(int i, int j, int[] anArray) {
+        int len = anArray.length;
+        //Проверка на переполнение
+        if ((i > len - 1) || (j > len - 1)) {
+            return false;
+        }
+        int buf;
+        buf = anArray[i];
+        anArray[i] = anArray[j];
+        anArray[j] = buf;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        //аргументы из командной строки
+        int i = Integer.parseInt(args[0]);
+        int j = Integer.parseInt(args[1]);
+        int[] arr = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+        System.out.println("Initial array: " + Arrays.toString(arr));
+        if (swap(i, j, arr)) {
+            System.out.println("Result:" + Arrays.toString(arr));
+        } else {
+            System.out.println("Out of bounds exception");
+        }
+
+    }
+}
