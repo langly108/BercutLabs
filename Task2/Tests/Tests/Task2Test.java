3b26ee0920fd0569a02286e2966385aa799cc031
chistyakov <langlydes@gmail.com>
2020-09-15 17:42:38 +0300

    tests

--- Task2/Tests/Tests/Task2Test.java
+++ Task2/Tests/Tests/Task2Test.java
@@ -9,12 +9,32 @@ import static org.junit.jupiter.api.Assertions.*;
 class Task2Test {
 
     @Test
-    void minNormal() {
+    void min1st() {
         int a, b, c;
         a = 10;
         b = 15;
         c = 20;
         int res = Task2.min(a, b, c);
         Assert.assertEquals(a, res);
+    }
+
+    @Test
+    void min2nd() {
+        int a, b, c;
+        a = 15;
+        b = 10;
+        c = 20;
+        int res = Task2.min(a, b, c);
+        Assert.assertEquals(b, res);
+    }
+
+    @Test
+    void min3rd() {
+        int a, b, c;
+        a = 15;
+        b = 20;
+        c = 10;
+        int res = Task2.min(a, b, c);
+        Assert.assertEquals(c, res);
     }
 }
